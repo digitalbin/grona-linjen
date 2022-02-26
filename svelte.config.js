a7b4426adeb52f8ec30c079494e255de594ecc98
Albin Hagström <albin.hagstrom@nentgroup.com>
2022-02-26 09:05:33 +0100

    He

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,15 +6,16 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
+		// paths: {
+		// 	base: '/vollie'
+		// },
 		adapter: adapter(),
-
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
 		}
-	}
+	},
 };
 
 export default config;
