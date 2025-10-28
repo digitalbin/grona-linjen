b395fde9f4b4311d3ceab43d82a70960b6e9ff15
Albin Hagström <albinhag@gmail.com>
2025-10-28 21:15:48 +0100

    redirect shop

--- src/api/stripe.ts
+++ src/api/stripe.ts
@@ -1,22 +1,27 @@
-import { query } from "@solidjs/router";
+import { query, redirect } from "@solidjs/router";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+const redirectProd = () => {
+  if (process.env.NODE_ENV !== "development") {
+    throw redirect("/");
+  }
+};
+
 export const getProducts = query(async () => {
   "use server";
-  const { data } = await stripe.products.list();
-  return data;
+  redirectProd();
+  const { data } = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  const h = Array.from({ length: 10 }, () => data[0]);
+  return h;
 }, "products");
 
 export const getProduct = query(async (id: string) => {
   "use server";
+  redirectProd();
   const product = await stripe.products.retrieve(id);
   return product;
 }, "products:id");
-
-export const getPrices = query(async () => {
-  "use server";
-  const { data } = await stripe.prices.list();
-  return data;
-}, "prices");
