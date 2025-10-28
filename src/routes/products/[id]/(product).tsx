5a7460b75c3687778303101b83d1d63a5ce7b8ca
Albin Hagström <albinhag@gmail.com>
2025-10-28 21:15:10 +0100

    start products page

--- src/routes/products/[id]/(product).tsx
+++ src/routes/products/[id]/(product).tsx
@@ -1,18 +1,17 @@
 import { getProduct } from "@/api/stripe";
 import { createAsync, RouteDefinition, useParams } from "@solidjs/router";
-import { createEffect } from "solid-js";
 
-// export const route = {
-//   preload: ({ params }) => getProduct(params.id),
-// } satisfies RouteDefinition;
+export const route = {
+  preload: ({ params }) => getProduct(params.id),
+} satisfies RouteDefinition;
 
 export default function ProductPage() {
   const params = useParams();
-  // const product = createAsync(() => getProduct(params.id));
+  const product = createAsync(() => getProduct(params.id));
   return (
     <div>
-      {/* <h1>{product()?.name}</h1>
-      <p>{product()?.description}</p> */}
+      <h1>{product()?.name}</h1>
+      <p>{product()?.description}</p>
     </div>
   );
 }
